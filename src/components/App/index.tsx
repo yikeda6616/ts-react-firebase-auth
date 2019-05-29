4870218c6f09842cba8857df7b10661e4386ea2a
Yasushi Ikeda <yikeda6616@gmail.com>
2019-05-29 11:14:01 -0700

    Add route path

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import * as ROUTES from '../../constants/routes';
 import Admin from '../Admin';
 import Account from '../Account';
 import Firebase from '../Firebase';
@@ -19,19 +20,22 @@ const App: React.FC = () => {
     <div>
       <Router>
         <Navigation />
+
+        <hr />
+
+        <Route exact path={ROUTES.LANDING} component={Landing} />
+        <Route path={ROUTES.SIGN_UP} component={SignUp} />
+        <Route path={ROUTES.SIGN_IN} component={SignIn} />
+        <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForget} />
+        <Route path={ROUTES.HOME} component={Home} />
+        <Route path={ROUTES.ACCOUNT} component={Account} />
+        <Route path={ROUTES.ADMIN} component={Admin} />
       </Router>
 
-      <Account />
-      <Admin />
       <Firebase />
-      <Home />
-      <Landing />
       <PasswordChange />
-      <PasswordForget />
       <Session />
-      <SignIn />
       <SignOut />
-      <SignUp />
     </div>
   );
 };
