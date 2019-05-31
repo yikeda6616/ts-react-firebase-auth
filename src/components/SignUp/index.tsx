e77ca43d377a0f0842070cc1bd8b9c388a4d8416
Yasushi Ikeda <yikeda6616@gmail.com>
2019-05-31 15:42:51 -0700

    Add hookState

--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import * as ROUTES from '../../constants/routes';
@@ -10,74 +10,59 @@ const SignUpPage: React.FC = () => (
   </div>
 );
 
-interface Props {}
-
-interface State {
-  username: string;
-  email: string;
-  passwordOne: string;
-  passwordTwo: string;
-  error: any;
-}
-
-const INITIAL_STATE: State = {
-  username: '',
-  email: '',
-  passwordOne: '',
-  passwordTwo: '',
-  error: null
-};
-
-export class SignUpForm extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = { ...INITIAL_STATE };
-  }
-
-  onSubmit = (event: React.FormEvent<HTMLElement>) => {};
-
-  onChange = (event: React.FormEvent<HTMLElement>) => {
-    this.setState({ [event.target.name]: event.target.value });
+export const SignUpForm = () => {
+  const INITIAL_STATE = {
+    username: '',
+    email: '',
+    passwordOne: '',
+    passwordTwo: '',
+    error: null
   };
 
-  render() {
-    const { username, email, passwordOne, passwordTwo, error } = this.state;
+  const [userInput, setUserInput] = useState(INITIAL_STATE);
 
-    return (
-      <form onSubmit={this.onSubmit}>
-        <input
-          type='text'
-          name='username'
-          value={username}
-          onChange={this.onChange}
-          placeholder='Full Name'
-        />
-        <input
-          type='text'
-          value={email}
-          onChange={this.onChange}
-          placeholder='Email Address'
-        />
-        <input
-          type='password'
-          name='passwordOne'
-          value={passwordOne}
-          onChange={this.onChange}
-          placeholder='Password'
-        />
-        <input
-          type='password'
-          name='passwordTwo'
-          value={passwordTwo}
-          onChange={this.onChange}
-          placeholder='Confirm Password'
-        />
-        <button type='submit'>Sign Up</button>
-        {error && <p>{error.message}</p>}
-      </form>
+  const onSubmit = (event: React.FormEvent<HTMLElement>) => {};
+
+  const onChange = (event: React.FormEvent<HTMLElement>) => {
+    setUserInput(
+      [event.target.name]: event.target.value
     );
-  }
-}
+  };
+
+  return (
+    <form onSubmit={() => onSubmit}>
+      <input
+        type='text'
+        name='username'
+        value={username}
+        onChange={() => onChange}
+        placeholder='Full Name'
+      />
+      <input
+        type='text'
+        value={email}
+        onChange={() => onChange}
+        placeholder='Email Address'
+      />
+      <input
+        type='password'
+        name='passwordOne'
+        value={passwordOne}
+        onChange={() => onChange}
+        placeholder='Password'
+      />
+      <input
+        type='password'
+        name='passwordTwo'
+        value={passwordTwo}
+        onChange={() => onChange}
+        placeholder='Confirm Password'
+      />
+      <button type='submit'>Sign Up</button>
+      {error && <p>{error.message}</p>}
+    </form>
+  );
+};
 
 export const SignUpLink: React.FC = () => (
   <p>
