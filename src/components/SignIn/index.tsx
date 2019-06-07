4c153da452d5c8fcfd3bb35713b02e6d8210a427
Yasushi Ikeda <yikeda6616@gmail.com>
2019-06-07 16:12:19 -0700

    Add userInput onChange

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FirebaseContext } from '../Firebase';
+import { SignUpLink } from '../SignUp';
 
 const SignInPage: React.FC = (props: any) => (
   <div>
@@ -7,14 +8,50 @@ const SignInPage: React.FC = (props: any) => (
     <FirebaseContext.Consumer>
       {firebase => <SignInForm firebase={firebase} history={props.history} />}
     </FirebaseContext.Consumer>
+    <SignUpLink />
   </div>
 );
 
+const INITIAL_STATE = {
+  email: '',
+  password: ''
+};
+
 const SignInForm = (props: any) => {
+  const [userInput, setUserInput] = useState(INITIAL_STATE);
+  const [error, setError] = useState('');
+
+  const onSubmit = (e: any) => {};
+
+  const onChange = (e: any) => {
+    setUserInput({ ...userInput, [e.target.name]: e.target.value });
+  };
+
+  const { email, password } = userInput;
+
+  const isInvalid = password === '' || email === '';
+
   return (
-    <h1>
-      <div>signin form</div>
-    </h1>
+    <form onSubmit={onSubmit}>
+      <input
+        name='email'
+        value={email}
+        onChange={onChange}
+        type='text'
+        placeholder='Email Address'
+      />
+      <input
+        name='password'
+        value={password}
+        onChange={onChange}
+        placeholder='Password'
+        type='text'
+      />
+      <button disabled={isInvalid} type='submit'>
+        Sign In
+      </button>
+      {error && <p>{error}</p>}
+    </form>
   );
 };
 export default SignInPage;
