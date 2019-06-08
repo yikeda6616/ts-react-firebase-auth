9c53257141c68181d648f4fcd3c7a74b5320dbb7
Yasushi Ikeda <yikeda6616@gmail.com>
2019-06-07 17:47:00 -0700

    Add useEffect to set authUser

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import * as ROUTES from '../../constants/routes';
 import AdminPage from '../Admin';
 import AccountPage from '../Account';
 import HomePage from '../Home';
@@ -12,27 +11,41 @@ import PasswordForgetPage from '../PasswordForget';
 import Session from '../Session';
 import SignInPage from '../SignIn';
 import SignUpPage from '../SignUp';
+import { FirebaseContext } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
 
-const App: React.FC = (props: any) => {
+const App = (props: any) => (
+  <FirebaseContext.Consumer>
+    {firebase => <AppBase {...props} firebase={firebase} />}
+  </FirebaseContext.Consumer>
+);
+
+const AppBase: React.FC = (props: any) => {
   const [authUser, setAuthUser] = useState(null);
 
-  return (
-    <div>
-      <Router>
-        <Navigation authUser={authUser} />
-        <hr />
-        <Route exact path={ROUTES.LANDING} component={LandingPage} />
-        <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-        <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-        <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-        <Route path={ROUTES.HOME} component={HomePage} />
-        <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-        <Route path={ROUTES.ADMIN} component={AdminPage} />
-      </Router>
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    props.firebase.auth.onAuthStateChanged((authUser: any) => {
+      authUser
+        ? setAuthUser({ authUser } as any)
+        : setAuthUser({ authUser: null } as any);
+    });
+  });
 
-      <PasswordChange />
-      <Session />
-    </div>
+  return (
+    <Router {...props}>
+      <Navigation authUser={authUser} />
+      <hr />
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+      <Route path={ROUTES.HOME} component={HomePage} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route path={ROUTES.ADMIN} component={AdminPage} />
+    </Router>
+    // <PasswordChange />
+    // <Session />
   );
 };
 
