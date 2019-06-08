43863dcafbd34ccd48d0d39dc5aa27a7c15a9d6c
Yasushi Ikeda <yikeda6616@gmail.com>
2019-06-07 17:18:13 -0700

    Add condition rendering

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -4,32 +4,38 @@ import { Link } from 'react-router-dom';
 import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
 
-const Navigation = (props: any) => (
-  <div>
-    <ul>
-      <li>
-        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.LANDING}>Landing</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.HOME}>Home</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ACCOUNT}>Account</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ADMIN}>Admin</Link>
-      </li>
-      <li>
-        <SignOutButton />
-      </li>
-    </ul>
-  </div>
+const Navigation = ({ authUser }: any) =>
+  authUser ? <NavigationAuth /> : <NavigationNonAuth />;
+
+const NavigationAuth: React.FC = () => (
+  <ul>
+    <li>
+      <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.HOME}>Home</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.ACCOUNT}>Account</Link>
+    </li>
+    <li>
+      <SignOutButton />
+    </li>
+  </ul>
+);
+
+const NavigationNonAuth: React.FC = () => (
+  <ul>
+    <li>
+      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
+  </ul>
 );
 
 export default Navigation;
