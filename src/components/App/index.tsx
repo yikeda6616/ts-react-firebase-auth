0a1bc92ac3bc1570fbc806ab89293a0dcaf42450
Yasushi Ikeda <yikeda6616@gmail.com>
2019-06-08 17:41:36 -0700

    Add comments about passing an empty array

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -27,7 +27,10 @@ const AppBase: React.FC = (props: any) => {
     props.firebase.auth.onAuthStateChanged((authUser: any) => {
       authUser ? setAuthUser({ authUser } as any) : setAuthUser(null);
     });
-    return () => {};
+    // Pass an empty array as a second argument.
+    // This tells React that the effect doesn't depend on any values from props or state, so it never needs to re-run.
+    // This way, the props and state inside the effect will always have their initial values.
+    // See more detail at https://reactjs.org/docs/hooks-effect.html
   }, []);
 
   return (
